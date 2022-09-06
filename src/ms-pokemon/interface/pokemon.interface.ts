62f53e36ad6f018c1a4cee51d8fd8dd671f4d53f
JesuaMunozP <110502620+JesuaMunozP@users.noreply.github.com>
2022-09-06 18:03:56 -0400

    Test Service

---
+++ src/ms-pokemon/interface/pokemon.interface.ts
@@ -0,0 +1,6 @@
+export interface Ipokemon {
+    id: string;
+    name: string;
+    type: string[];
+  }
+  
--- src/ms-pokemon/test/ms-pokemon.service.spec.ts
+++ src/ms-pokemon/test/ms-pokemon.service.spec.ts
@@ -1,18 +1,59 @@
+import { HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
+import { of } from 'rxjs';
 import { MsPokemonService } from '../ms-pokemon.service';
 
 describe('MsPokemonService', () => {
   let service: MsPokemonService;
+  let httpService: HttpService;
+
+  const mockRepository = {
+    get: jest.fn(),
+    findOne: jest.fn(),
+    serviceCallPokemon: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MsPokemonService],
+      providers: [
+        MsPokemonService,
+        {
+          provide: HttpService,
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<MsPokemonService>(MsPokemonService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('should return a pokemon', () => {
+    it('serviceCallPokemon(), should return a pokemon', async () => {
+      const pokemon = [{
+        id: '1',
+        name: 'pokemon',
+        type: ['1', '2'],
+      }];
+      jest.spyOn(httpService, 'get').mockReturnValueOnce(
+        of({
+          status: 200,
+          statusText: 'OK',
+          config: {},
+          headers: {},
+          data: pokemon,
+        }),
+      );
+
+      await mockRepository.serviceCallPokemon.mockReturnValue(pokemon);
+      mockRepository.findOne.mockReturnValue(pokemon);
+      const resultPokemon = await service.findOne('1');
+     // const resultCallPokemon = await service.serviceCallPokemon('url', '1');
+      // expect(mockRepository.findOne).toHaveBeenCalledTimes(1);
+      // expect(mockRepository.serviceCallPokemon).toBeCalledTimes(1);
+      // expect(await service.findOne('1')).toBeCalledTimes(1);
+      // expect(resultPokemon).toEqual(expected);
+      // expect(resultCallPokemon).toContain(pokemon);
+    });
   });
 });
